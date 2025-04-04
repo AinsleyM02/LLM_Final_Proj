206cffcd8c176ebaf12e10fd7621c09de8411c08
charbelmarche33 <cmarche@mail.umw.edu>
2025-04-03 20:26:16 -0400

    Added comments to chunking function

--- utils.py
+++ utils.py
@@ -21,7 +21,7 @@ from sentence_transformers import SentenceTransformer
 embedding_model = SentenceTransformer(EMBEDDING_MODEL)
 
 
-def simple_sentence_splitter(text):
+def sentence_splitter(text):
     return re.split(r"(?<=[.!?])\s+", text.strip())
 
 
@@ -36,22 +36,31 @@ def embed_text(text: str, max_chunk_size: int = 256) -> np.array:
     Returns:
     - np.array, stacked embeddings of all chunks
     """
-    sentences = simple_sentence_splitter(text)
-    chunks = []
-    current_chunk = []
+    sentences = sentence_splitter(text)
+    chunks = []  # A list of all chunks
+    current_chunk = []  # A list of sentences in the current chunk
 
+    # Go through the sentences and add them to the current chunk
     for sentence in sentences:
         current_chunk.append(sentence)
+        # Until the current chunk is too big, then we remove the last sentence and append the current chunk to the list of chunks
         if len(" ".join(current_chunk).split()) > max_chunk_size:
             # Remove last sentence if it exceeds limit
             current_chunk.pop()
             chunks.append(" ".join(current_chunk))
-            current_chunk = [sentence]
+            current_chunk = [sentence]  # Now start a new chunk with the last sentence
 
+    # Add the last chunk if it exists
+    # If the last chunk is empty, then we don't need to add it to the list of chunks
     if current_chunk:
         chunks.append(" ".join(current_chunk))
 
+    # Now we have a list of chunks, let's embed them
+    # We can use the embedding model to encode the chunks
     embeddings = [embedding_model.encode(chunk) for chunk in chunks]
+
+    # Now we have a list of embeddings, let's stack them
+    # We can use np.vstack to stack the embeddings
     return np.vstack(embeddings)
 
 
